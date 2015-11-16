9cb068ae4cdc2d640e219f8179f5fa66a04a39fc
Administrator <Administrator@2-510-23>
2015-11-16 15:58:26 +0900

    j

--- NoTest/src/com/example/notest/MainActivity.java
+++ NoTest/src/com/example/notest/MainActivity.java
@@ -1,120 +1,54 @@
 package com.example.notest;
 
 import android.app.Activity;
-import android.content.Context;
+import android.content.BroadcastReceiver;
+import android.content.Intent;
+import android.location.GpsStatus.Listener;
 import android.media.AudioManager;
 import android.os.Bundle;
-import android.os.Handler;
-import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 
 public class MainActivity extends Activity {
-	Handler hd = new Handler();
-	private AudioReader audioReader;
-	private int sampleRate = 8000;
-	private int inputBlockSize = 256;
-	private int sampleDecimate = 1;
-	TextView text;
-	int interval = 60;
-	int[] ac; // 데시벨 저장
-	int sum, avg;
-	AudioManager am;
 
+	public static TextView text;
+	Intent i=new Intent();
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-
-        
-        
-		audioReader = new AudioReader();
 		setVolumeControlStream(AudioManager.STREAM_MUSIC);
-		text = (TextView) findViewById(R.id.textView1);
-		am = (AudioManager) getSystemService(Context.AUDIO_SERVICE);
+		i.setAction("com.example.notest.re");
+		text =(TextView) findViewById(R.id.textView1);
 
 	}
 
-	public void doStart(View v) {
-		ac = new int[interval];
-		audioReader.startReader(sampleRate, inputBlockSize * sampleDecimate,
-				new AudioReader.Listener() {
-					@Override
-					public final void onReadComplete(int dB) {
-						receiveDecibel(dB);
-						ac[0] = dB + 73; // 데시벨이 -73~70 돼서 나오므로 더해줌
-						hd.post(new Runnable() {
+	public void onClick(View v) {
+		
+		switch (v.getId()) {
+		// 1. 카운트 시작
+		case R.id.start: {
+			Intent serviceIntent = new Intent("com.example.notest.NoService");
+			startService(serviceIntent);
+			
+			
+			break;
+		}
 
-							@Override
-							public void run() {
-								if(ac[0]>0){
-								text.setText(ac[0] + " dB"); // 앱에 데시벨 표시
-								}
-								// 매초마다 데시벨 저장
-								for (int j = ac.length - 1; j > 0; j--) {
-									ac[j] = ac[j - 1];
-								}
-								sum = 0;
+		// 2. 카운트 종료
+		case R.id.end: {
+			Intent serviceIntent = new Intent("com.example.notest.NoService");
+			stopService(serviceIntent);
+			break;
+		}
 
-								// 데시벨 평균
-								for (int i = 0; i < ac.length; i++) {
-									if (ac[i] > 0) {
-										sum += ac[i];
-									}
-								}
-								avg = sum / interval;
-
-								for (int i = 0; i < 6; i++) {
-									System.out.println(ac[i * 10] + " avg : "
-											+ avg);
-								}
-
-								// 상태변화
-								switch (am.getRingerMode()) {
-								case AudioManager.RINGER_MODE_SILENT:
-									Log.i("MyApp", "Silent mode");
-									break;
-								case AudioManager.RINGER_MODE_VIBRATE:
-									Log.i("MyApp", "Vibrate mode");
-									break;
-								case AudioManager.RINGER_MODE_NORMAL:
-									Log.i("MyApp", "Normal mode");
-									if (avg < 30) {
-										am.setRingerMode(AudioManager.RINGER_MODE_SILENT);
-									}
-									break;
-
-								}
-
-							}
-						});
-					}
-
-					@Override
-					public void onReadError(int error) {
-					}
-				});
+		}
+		
 	}
 
-	public void amove() {
-
-	}
-
-	public void doEnd(View v) {
-		audioReader.stopReader();
-	}
-
-	private void receiveDecibel(final int dB) {
-		Log.e("###", dB + " dB"); // Logcat에 데시벨이 -70~ -73으로 출력이되어 73을더한다.
-	}
-
-	
 	@Override
 	protected void onDestroy() {
-		// TODO Auto-generated method stub
-		audioReader.stopReader();
 		super.onDestroy();
 	}
-
 
 }
--- NoTest/src/com/example/notest/NoService.java
+++ NoTest/src/com/example/notest/NoService.java
@@ -3,15 +3,35 @@ package com.example.notest;
 import android.app.Notification;
 import android.app.PendingIntent;
 import android.app.Service;
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
+import android.media.AudioManager;
+import android.os.Bundle;
+import android.os.Handler;
 import android.os.IBinder;
 import android.util.Log;
+import android.widget.Toast;
 
 public class NoService extends Service{
+	Handler hd = new Handler();
+	private AudioReader audioReader;
+	private int sampleRate = 8000;
+	private int inputBlockSize = 256;
+	private int sampleDecimate = 1;
+	int interval = 60;
+	int[] ac; // 데시벨 저장
+	int sum, avg;
+	AudioManager am;
     public void onCreate()
     {
         super.onCreate();
         Log.i("superdroid", "onCreate()");
+        
+        audioReader = new AudioReader();
+		
+		am = (AudioManager) getSystemService(Context.AUDIO_SERVICE);
         
         Intent intent = new Intent(this, MainActivity.class );
         PendingIntent pIntent = PendingIntent.getActivity( this, 0, intent, 
@@ -32,12 +52,74 @@ public class NoService extends Service{
     {
         super.onStartCommand( intent, flags, startId );
         Log.i("superdroid", "onStartCommand()");
+    	ac = new int[interval];
+		audioReader.startReader(sampleRate, inputBlockSize * sampleDecimate,
+				new AudioReader.Listener() {
+					@Override
+					public final void onReadComplete(int dB) {
+						ac[0] = dB + 73; // 데시벨이 -73~70 돼서 나오므로 더해줌
+						hd.post(new Runnable() {
+
+							@Override
+							public void run() {
+								
+								if(ac[0]>0){
+								   
+
+								};
+								//text.setText(ac[0] + " dB"); // 앱에 데시벨 표시
+								// 매초마다 데시벨 저장
+								for (int j = ac.length - 1; j > 0; j--) {
+									ac[j] = ac[j - 1];
+								}
+								sum = 0;
+
+								// 데시벨 평균
+								for (int i = 0; i < ac.length; i++) {
+									if (ac[i] > 0) {
+										sum += ac[i];
+									}
+								}
+								avg = sum / interval;
+
+								for (int i = 0; i < 6; i++) {
+									System.out.println(ac[i * 10] + " avg : "
+											+ avg);
+								}
+
+								// 상태변화
+								switch (am.getRingerMode()) {
+								case AudioManager.RINGER_MODE_SILENT:
+									Log.i("MyApp", "Silent mode");
+									break;
+								case AudioManager.RINGER_MODE_VIBRATE:
+									Log.i("MyApp", "Vibrate mode");
+									break;
+								case AudioManager.RINGER_MODE_NORMAL:
+									Log.i("MyApp", "Normal mode");
+									if (avg < 30) {
+										am.setRingerMode(AudioManager.RINGER_MODE_SILENT);
+									}
+									break;
+
+								}
+
+							}
+						});
+					}
+
+					@Override
+					public void onReadError(int error) {
+					}
+				});
+        
         return START_STICKY; 
     }
 
     public void onDestroy()
     {
         Log.i("superdroid", "onDestroy()");
+        audioReader.stopReader();
         super.onDestroy();
     }
 
