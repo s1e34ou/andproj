bb8adf3fa1244242f8d91ab89bf91c74c3a0e1a6
Administrator <Administrator@2-510-24>
2015-11-18 17:55:36 +0900

    111111

--- NoTest/gen/com/example/notest/R.java
+++ NoTest/gen/com/example/notest/R.java
@@ -48,11 +48,13 @@ public final class R {
     }
     public static final class layout {
         public static final int activity_activity__test=0x7f030000;
+        public static final int activity_help=0x7f030001;
     }
     public static final class menu {
         public static final int activity__test=0x7f080000;
-        public static final int main=0x7f080001;
-        public static final int test=0x7f080002;
+        public static final int help1=0x7f080001;
+        public static final int main=0x7f080002;
+        public static final int test=0x7f080003;
     }
     public static final class string {
         public static final int action_settings=0x7f050002;
@@ -60,6 +62,7 @@ public final class R {
         public static final int hello_world=0x7f050001;
         public static final int title_activity_activity__test=0x7f050004;
         public static final int title_activity_help=0x7f050005;
+        public static final int title_activity_help1=0x7f050006;
         public static final int title_activity_test=0x7f050003;
     }
     public static final class style {
